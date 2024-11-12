dbe5f0623198b11fe94d34f8ae59a0bb3222e1a2
SANTHOSH-MAMIDISETTI <mamidisettisanthosh2004@gmail.com>
2024-11-12 12:54:09 +0530

    minor settings change

--- robotics_club_cms/settings/base.py
+++ robotics_club_cms/settings/base.py
@@ -23,7 +23,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-22f(5tp(-i+@e5+5h$xvgis5ls1s)*k2j+araj%+y6qn^a03x*'
 
-# SECURITY WARNING: don't run with debug turned on in production!
 
 # Application definition
 
